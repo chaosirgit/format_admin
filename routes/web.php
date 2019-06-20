4d5602ece094e96196cd8228781be55ebf8e7ade
chaosir <qq278935403@gmail.com>
2019-06-20 15:40:24 +0800

    chaosir

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,5 @@
 */
 
 Route::group(['prefix' => 'api/v1', 'middleware' => ['wechat.oauth']],function(){
-    Route::post('login','Api\UserController@login');
+    Route::get('login','Api\UserController@login');
 });
