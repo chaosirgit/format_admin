9cc4a79358b8098562c0882953320228ceaa392e
chaosir <qq278935403@gmail.com>
2019-06-20 15:24:52 +0800

    chaosir

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -7,8 +7,9 @@ use App\Http\Controllers\Controller;
 
 class UserController extends Controller
 {
-    public function login(){
+    public function login(Request $request){
         $user_id = session('user_id');
+        return redirect($request->back_url);
         return $this->success($user_id);
     }
 }
