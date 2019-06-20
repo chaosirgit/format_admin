64d0c516bb4752270fdb0c9f60918554c76d4c62
chaosir <qq278935403@gmail.com>
2019-06-20 15:20:31 +0800

    chaosir

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -57,6 +57,7 @@ class Kernel extends HttpKernel
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
+        'OriginApi' => \App\Http\Middleware\OriginApi::class,
         'wechat.oauth' => \App\Http\Middleware\WechatAuth::class,
 
     ];
