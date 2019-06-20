4e691637b79f41e331fc62f8ba0872ddcb9b71c8
chaosir <qq278935403@gmail.com>
2019-06-20 20:43:12 +0800

    chaosir

--- app/Http/Middleware/WechatAuth.php
+++ app/Http/Middleware/WechatAuth.php
@@ -52,6 +52,7 @@ class WechatAuth
                         DB::commit();
                     }
                     Redis::connection('token')->set($user->id,md5($user->uid.time()));
+                    Redis::connection('token')->set(md5($user->uid.time()),$user->id);
                     $user_id = $user->id;
                 }
             }
