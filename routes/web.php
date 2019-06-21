6112cc0a6542a3e8b72ad7f014bb8f35d0b0ccfd
chaosir <qq278935403@gmail.com>
2019-06-21 16:50:10 +0800

    chaosir

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,8 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('admin/login','Admin\AdminController@login');//后台登录页
+Route::post('admin/login','Admin\AdminController@postLogin');//后台登录接口
 
 Route::group(['prefix' => 'api/v1', 'middleware' => ['wechat.oauth']],function(){
     Route::post('login','Api\UserController@login');
