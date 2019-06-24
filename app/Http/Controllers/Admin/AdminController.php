d6ff7a1d544d43362d5fcb8949790bed952c789e
chaosir <qq278935403@gmail.com>
2019-06-24 19:11:07 +0800

    chaosir

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -11,8 +11,7 @@ class AdminController extends Controller
 {
     public function login(){
         $project_name = env('PROJECT_NAME','模因科技');
-        var_dump(session(['aaa'=>1]));
-        var_dump(session('aaa'));
+
         return view('admin.login')->with('project_name',$project_name);
     }
 
