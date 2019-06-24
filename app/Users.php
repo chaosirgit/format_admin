89117bac80728dfcb94efb1c6c7671719dc52563
chaosir <qq278935403@gmail.com>
2019-06-24 20:23:32 +0800

    chaosir

--- app/Users.php
+++ app/Users.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Redis;
 use Illuminate\Support\Facades\Request;
@@ -18,6 +19,27 @@ class Users extends Model
 {
     protected $table = 'users';
     public $timestamps = false;
+    protected $hidden = ['radar_password','radar_pay_password'];
+    protected $appends = ['parent_mobile'];
+
+    public static function generatePassword($password)
+    {
+        $salt          = env('PASSWORD_SALT','PAYBAL');
+        $passwordChars = str_split($password);
+
+        foreach ($passwordChars as $char) {
+            $salt .= md5($char);
+        }
+        return md5($salt);
+    }
+
+    public function getParentMobileAttribute(){
+        return self::find($this->attributes['parent_id'])->mobile ?? '暂无';
+    }
+
+    public function getCreateTimeAttribute(){
+        return Carbon::createFromTimestamp($this->attributes['create_time'])->toDateTimeString();
+    }
 
     public static function getUserByOpenId($open_id){
         return self::where('open_id',$open_id)->first();
