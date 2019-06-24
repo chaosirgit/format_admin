967829de10829f95af308ae2f638e173ea416597
chaosir <qq278935403@gmail.com>
2019-06-24 21:07:41 +0800

    chaosir

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -77,7 +77,7 @@ class Controller extends BaseController
 
     public function localUpload(Request $request){
         $file = $request->file('file');
-        var_dump($file->getExtension());die;
+        var_dump($file->extension());die;
         $path = $request->file('file')->store('public');
         var_dump($path);die;
         $path = str_replace('public','storage',$path);
